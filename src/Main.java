a9d9a661eb903e67bcadb455ae35e0d84fe3d764
MatcheyGradient <benjaminlewith1e@gmail.com>
2022-01-29 12:52:40 -0700

    numbers

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import math.Numbers;
 import math.Triangle;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Numbers.factorNumber(7));
+        System.out.println(Arrays.toString(Numbers.fibonacci(2, 1, 20))); // lucas numbers
     }
 }
--- src/math/Numbers.java
+++ src/math/Numbers.java
@@ -2,12 +2,30 @@ package math;
 
 public class Numbers {
 
-    public static int factorNumber(int number){
-        int store = 1;
+    public static long factorNumber(int number){
+        long store = 1;
         for(int i = number; i > 0; i--){
             store = store * i;
         }
-
         return store;
+    }
+
+    public static long[] fibonacci(int seed1, int seed2, int length){ // breaks at a length of 93 (too big)
+        long[] series = new long[length];
+
+        long a = seed1;
+        long b = seed2;
+        
+        for(int i = 0; i < length; i++){
+            long c = a + b;
+
+            series[i] = a;
+
+            a = b;
+            b = c;
+        }
+
+
+        return series;
     }
 }
